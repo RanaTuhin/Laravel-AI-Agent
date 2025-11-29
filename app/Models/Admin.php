fca613eeea597a1f82d21458021176a032151502
Tuhin Rana Mondal <aimlesstuhin@gmail.com>
2025-11-30 00:40:01 +0530

    Setup Filament for Admin

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -1,11 +1,18 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Model;
+use Filament\Panel;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+class Admin extends Authenticatable implements FilamentUser
 {
-    //
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 }
